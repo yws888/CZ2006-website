5bef0faa91c4a58556d78e3e9a8f059554307b38
yws888 <61619668+yws888@users.noreply.github.com>
2021-03-06 18:32:08 +0800

    Initial commit

---
+++ CZ2006/househunt/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='househunt-home'),
+    path('about/', views.about, name='househunt-about'),
+    path('calculate/',views.calculate, name='househunt-calculate'),
+    path('calculate/result/',views.result, name='househunt-result'),
+
+]
---
+++ CZ2006/househunt/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+#from django.http import HttpResponse
+
+# Create your views here.
+# posts = [
+#     {
+#         'author': 'CoreyMS',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 27, 2018'
+#     },
+#     {
+#         'author': 'Jane Doe',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 28, 2018'
+#     }
+# ]
+
+
+def home(request):
+    context = { #to pass info to template; can accses data within that template
+        #'posts': posts, #info assoc w each post
+        'title': 'Home'
+    }
+    return render(request, 'househunt/home.html', context)
+
+def about(request):
+    return render(request, 'househunt/about.html', {'title': 'About'}) #add title if u want a title for the page
+
+def calculate(request):
+    return render(request, "househunt/calculate.html", {'title': 'Calculate'})
+
+def result(request):
+    monthlyIncome = request.POST['num1']
+    savings = request.POST['num2']
+    cpfBalance = request.POST['num3']
+
+
+    if monthlyIncome.isdigit() and savings.isdigit():
+        a = int(monthlyIncome)
+        b = int(savings)
+        c = int(cpfBalance)
+        res = a*12 + b + c
+        valid = True
+
+        return render(request, "househunt/result.html", {"result": res, "valid": valid})
+    else:
+        res = "Only digits are allowed"
+        valid = False
+        return render(request, "househunt/result.html", {"result": res, "valid": valid})
