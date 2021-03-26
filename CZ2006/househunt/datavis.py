76fc78e0591b2275ecaff800d56d276fa5dac432
VSCodeOnly <73070369+VSCodeOnly@users.noreply.github.com>
2021-03-26 14:31:06 +0800

    Implemented as Strategy Pattern
    References: https://refactoring.guru/design-patterns/strategy/python/example

--- CZ2006/househunt/datavis.py
+++ CZ2006/househunt/datavis.py
@@ -1,6 +1,8 @@
 """
 Data Visualisation Module
 """
+from __future__ import annotations
+from abc import ABC, abstractmethod
 import os
 import numpy as np
 import pandas as pd
@@ -12,44 +14,83 @@ sb.set(rc={'figure.figsize':(12.7,9.27)})
 pd.set_option('display.max_column',None)
 pd.set_option('display.max_rows',None)
 
+'''
+Reads data then visualises the data depending on user choice
+'''
+class readData():
+    # constructor
+    def __init__(self, strategy: DataVisualisation):
+        self._strategy = strategy
 
-# read data from a csv file
-def readData():
-    # columns to read from file
-    col_list = ['resale_price','town','flat_type','month']
-    fileName = os.path.join(os.getcwd(), "CZ2006", "utility","resale-flat-prices.csv")
-    csvData = pd.read_csv(fileName, usecols=col_list)
-    return pd.DataFrame(csvData)
+    def strategy(self):
+        return self._strategy
 
-# display a bar graph of the flat's resale price against the town
-def barPriceVsTown(csvData):
-    save_location = os.path.join(os.getcwd(), "CZ2006", "househunt", "static", "town.png")
-    if not os.path.exists(save_location):
-        sb.barplot(x='resale_price', y='town', data = csvData, ci=None)
-        # save plot to PNG for use in HTML
-        plt.savefig(save_location, bbox_inches = "tight", dpi = 100)
-        #fig.savefig("CZ2006\\househunt\\static\\town.png", bbox_inches = "tight", dpi = 100)
-        plt.close()
+    def strategy(self, strategy: DataVisualisation):
+        self._strategy = strategy
+
+    # read data from a csv file
+    def dataToGraph(self):
+        # columns to read from file
+        col_list = ['resale_price','town','flat_type','month']
+        fileName = os.path.join(os.getcwd(), "CZ2006", "utility","resale-flat-prices.csv")
+        csvData = pd.read_csv(fileName, usecols=col_list)
+        self._strategy.displayGraph(csvData)
+
+'''
+Strategy Pattern
+'''
+class DataVisualisation(ABC):
+    @abstractmethod
+    def displayGraph(self, data):
+        pass
+
+'''
+ConcreteStrategyA
+Display a bar graph of the flat's resale price against the town
+'''
+class barPriceVsTown(DataVisualisation):
+    def displayGraph(self, data):    
+        save_location = os.path.join(os.getcwd(), "CZ2006", "househunt", "static", "town.png")
+        if not os.path.exists(save_location):
+            sb.barplot(x='resale_price', y='town', data = data, ci=None)
+            plt.xlabel("Year of sale")
+            plt.xlabel("Town")
+            # save plot to PNG for use in HTML
+            plt.savefig(save_location, bbox_inches = "tight", dpi = 100)
+            #fig.savefig("CZ2006\\househunt\\static\\town.png", bbox_inches = "tight", dpi = 100)
+            plt.close()
     
-# display a bar graph of the flat's resale price against the flat type
-def barPriceVsFlatType(csvData):
-    save_location = os.path.join(os.getcwd(), "CZ2006", "househunt", "static", "flat_type.png")
-    if not os.path.exists(save_location):
-        home_types = ["1 ROOM", "2 ROOM", "3 ROOM", "4 ROOM", "5 ROOM", "EXECUTIVE", "MULTI-GENERATION"]
-        sb.barplot(x='flat_type', y='resale_price', data=csvData, ci=None, order = home_types)
-        # save plot to PNG for use in HTML    
-        plt.savefig(save_location, bbox_inches = "tight", dpi = 100)
-        #fig.savefig('CZ2006\\househunt\\static\\flat_type.png', bbox_inches = "tight", dpi = 100)
-        plt.close()
+'''
+ConcreteStrategyB
+Display a bar graph of the flat's resale price against the flat type
+'''
+class barPriceVsFlatType(DataVisualisation):
+    def displayGraph(self, data):
+        save_location = os.path.join(os.getcwd(), "CZ2006", "househunt", "static", "flat_type.png")
+        if not os.path.exists(save_location):
+            home_types = ["1 ROOM", "2 ROOM", "3 ROOM", "4 ROOM", "5 ROOM", "EXECUTIVE", "MULTI-GENERATION"]
+            sb.barplot(x='flat_type', y='resale_price', data=data, ci=None, order = home_types)
+            plt.xlabel("Year of sale")
+            plt.xlabel("Flat type")
+            # save plot to PNG for use in HTML    
+            plt.savefig(save_location, bbox_inches = "tight", dpi = 100)
+            #fig.savefig('CZ2006\\househunt\\static\\flat_type.png', bbox_inches = "tight", dpi = 100)
+            plt.close()
 
-# display a point plot of the flat's resale price against the year the resale occurred
-def pointPriceVsYear(csvData):
-    save_location = os.path.join(os.getcwd(), "CZ2006", "househunt", "static", "year.png")
-    if not os.path.exists(save_location):
-        # convert month to year
-        csvData['year']=csvData['month'].str[0:4]
-        sb.lineplot(x='year', y='resale_price', data=csvData, ci=None)
-        # save plot to PNG for use in HTML
-        plt.savefig(save_location, bbox_inches = "tight", dpi = 100)
-        # fig.savefig('CZ2006\\househunt\\static\\year.png', bbox_inches = "tight", dpi = 100)
-        plt.close()
+'''
+ConcreteStrategyC
+Display a point plot of the flat's resale price against the year the resale occurred
+'''
+class pointPriceVsYear(DataVisualisation):
+    def displayGraph(self, data):
+        save_location = os.path.join(os.getcwd(), "CZ2006", "househunt", "static", "year.png")
+        if not os.path.exists(save_location):
+            # convert month to year
+            data['year']=data['month'].str[0:4]
+            sb.lineplot(x='year', y='resale_price', data=data, ci=None)
+            plt.xlabel("Year of sale")
+            plt.xlabel("Resale Price")
+            # save plot to PNG for use in HTML
+            plt.savefig(save_location, bbox_inches = "tight", dpi = 100)
+            # fig.savefig('CZ2006\\househunt\\static\\year.png', bbox_inches = "tight", dpi = 100)
+            plt.close()
