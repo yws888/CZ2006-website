9fe50a8f34657a12222980a6b5cd5ec0c331ef52
yws888 <61619668+yws888@users.noreply.github.com>
2021-03-26 17:54:49 +0800

    reloaded db w appropriate data

--- CZ2006/utility/load_db.py
+++ CZ2006/utility/load_db.py
@@ -17,5 +17,6 @@ with open(os.path.join(os.getcwd(), "CZ2006", "utility","resale-flat-prices.csv"
     next(reader)  # Advance past the header
 
     for row in reader:
-        templcY = int(2021 - int(row[8])) #calculated by subtracting leaseCommencementYear from 2021
-        HDBResaleFlat.objects.create(monthOfSale = row[0], town = row[1], flatType = row[2], blockNo = row[3], streetName = row[4], storeyRange = row[5], floorArea= row[6], flatModel = row[7], leaseCommencementYear = row[8], remainingLease = templcY, resalePrice = row[10] )
+        #templcY = int(2021 - int(row[8])) #calculated by subtracting leaseCommencementYear from 2021
+        remainingLease = int(row[9][:2])
+        HDBResaleFlat.objects.create(monthOfSale = row[0], town = row[1], flatType = row[2], blockNo = row[3], streetName = row[4], storeyRange = row[5], floorArea= row[6], flatModel = row[7], leaseCommencementYear = row[8], remainingLease = remainingLease, resalePrice = row[10] )
